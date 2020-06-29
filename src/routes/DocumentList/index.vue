a783f8805c259033083f72a1d30d3c9f0544ec95
Kiril Stas <vesground@gmail.com>
2020-06-29 11:42:45 +0300

    Add document list layout

--- src/routes/DocumentList/index.vue
+++ src/routes/DocumentList/index.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>Documents list</h1>
-    <ol>
-      <li v-for='text in texts' v-bind:key="text.id">
-        <router-link v-bind:to="{ name: 'document.edit', params: { id: text.id }}">
-          {{ text.preview }}
-        </router-link>
-      </li>
-    </ol>
+  <div class="document-list">
+    <div class='header'>
+      <TextField>Documents</TextField>
+      <Button :handleClick="openModal">Create New Document</Button>
+    </div>
+    <List :items='texts' :icon="icon" :handleClick='openDocumentDetails' />
     <div>
       <textarea v-model="newText" placeholder="paste text"></textarea>
       <button v-on:click="saveText">Save</button>
@@ -16,26 +13,38 @@
 </template>
 
 <script>
-import { listTexts, createText } from 'service/api.js';
+  import TextField from 'components/Text/index.vue';
+  import Button from 'components/Button/index.vue';
+  import List from 'components/List/index.vue';
+  import DocumentIcon from 'icons/DocumentIcon.vue';
+  import { listTexts, createText } from 'service/api.js';
 
-export default {
-  name: 'DocmentList',
-  async created() {
-    const response = await listTexts();
-    this.texts = response.data.texts;
-  },
-  data: () => ({
-    texts: [],
-    newText: ''
-  }),
-  methods: {
-    async saveText() {
-      const response = await createText(this.newText);
-      this.newText = '';
-      this.$router.push(`text/${response.data.id}`);
+  export default {
+    name: 'DocmentList',
+    async created() {
+      const response = await listTexts();
+      this.texts = response.data.texts;
+    },
+    computed: {
+      icon: () => ({ name: 'documentList', body: DocumentIcon })
+    },
+    data: () => ({
+      texts: [],
+      newText: ''
+    }),
+    components: { TextField, Button, List },
+    methods: {
+      async saveText() {
+        const response = await createText(this.newText);
+        this.newText = '';
+        this.$router.push(`text/${response.data.id}`);
+      },
+      openModal() {},
+      openDocumentDetails(documentId) {
+        this.$router.push(`text/${documentId}`);
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->

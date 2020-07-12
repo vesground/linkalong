880d3e1294703e0c4d8b272660ae1d8a38f4b17e
Kiril Stas <vesground@gmail.com>
2020-07-12 18:29:15 +0300

    Mocked browser on server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const { createBundleRenderer } = require('vue-server-renderer');
+const { JSDOM } = require('jsdom');
 
 const template = require('fs').readFileSync(
   path.join(__dirname, '../config/index.html'),
@@ -11,6 +12,14 @@ const serverBundle = require('../dist/vue-ssr-server-bundle.json');
 const clientManifest = require('../dist/vue-ssr-client-manifest.json');
 
 const server = express();
+
+// Mock the browser for node
+const DOM = new JSDOM(template);
+global.window = DOM.window;
+global.document = DOM.window.document;
+global.navigator = {
+	userAgent: 'node.js',
+};
 
 const renderer = createBundleRenderer(serverBundle, {
   runInNewContext: false,
